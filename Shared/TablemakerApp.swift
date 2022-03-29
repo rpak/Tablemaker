7a2a2915604759fc132e27b1df8aa25025800a24
Rob Pak <rob@carbonfive.com>
2022-03-28 20:51:03 -0700

    initial impound

---
+++ Shared/TablemakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TablemakerApp.swift
+//  Shared
+//
+//  Created by Rob Pak on 3/28/22.
+//
+
+import SwiftUI
+
+@main
+struct TablemakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
